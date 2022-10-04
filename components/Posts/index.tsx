fa64ac84f64c970e0b4a3d6b88d1792f2cfe7028
parallax-robin <robin@parallaxlogic.net>
2022-10-04 13:55:26 +0600

    ss

--- components/Posts/index.tsx
+++ components/Posts/index.tsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+
   return (
     <>
       <h1>All Posts</h1>
     </>
   );
 };
+
+export const getServerSideProps = async () => {};
 
 export default Posts;
